187060dcbbd5f4496f6e66f33162a3b8ef1967c3
Alexander Nevsky <63001556+alexandernevsky@users.noreply.github.com>
2025-12-05 17:23:17 +0400

    Changes

--- .eleventy.js
+++ .eleventy.js
@@ -78,6 +78,9 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("static/css");
+  eleventyConfig.addPassthroughCopy("static/js");
+  eleventyConfig.addPassthroughCopy("static/fonts");
   eleventyConfig.addPassthroughCopy("admin/");
   // We additionally output a copy of our CSS for use in Decap CMS previews
   eleventyConfig.addPassthroughCopy("_includes/assets/css/inline.css");
